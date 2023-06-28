be8694b19f62cb1440433ba08b166f17c995107b
Squex0978 <123485001+Squex0978@users.noreply.github.com>
2023-06-28 13:25:47 +0200

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+echo 'Teamspeak Installer Linux'
+
+apt update
+
+apt upgrade -y
+
+mkdir -p /home/ts3
+
+cd /home/ts3
+
+wget https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+
+ls
+
+tar xfvj teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+
+rm teamspeak3-server_linux_amd64-3.13.7.tar.bz2
