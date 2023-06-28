a593aea446b2caadc988af0cf190168a079c84ac
Squex0978 <123485001+Squex0978@users.noreply.github.com>
2023-06-28 17:37:22 +0200

    Update host.sh

--- host.sh
+++ host.sh
@@ -17,3 +17,7 @@ ls
 tar xfvj teamspeak3-server_linux_amd64-3.13.7.tar.bz2
 
 rm teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+
+cd teamspeak3-server_linux_amd64
+
+touch .ts3server_license_accepted
